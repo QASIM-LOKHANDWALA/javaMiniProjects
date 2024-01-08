acd458247acb888db72fd471019c2419e392e09d
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2024-01-08 16:53:10 +0530

    ADDED GUESS THE NUMBER

--- GameZone/Run.java
+++ GameZone/Run.java
@@ -4,51 +4,67 @@ import java.util.*;
 public class Run {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Player 1 Name: ");
-        String name1 = sc.nextLine();
-        System.out.println("Enter Player 1 Mark: ");
-        String temp = sc.nextLine().toUpperCase();
-        char m1 = temp.charAt(0);
-        System.out.println("Enter Player 2 Name: ");
-        String name2 = sc.nextLine();
-        char m2;
-        if(m1 == 'X'){
-            m2 = 'O';
-        }else{
-            m2 = 'X';
-        }
+        int choice;
+        System.out.println("\nWHAT WOULD YOU LIKE TO PLAY: ");
+        System.out.println("ENTER 1 : TO PLAY TIC TAC TOE");
+        System.out.println("ENTER 2 : TO PLAY GUESS THE NUMBER");
+        System.out.print("\nENTER YOUR CHOICE : ");
+        choice = sc.nextInt();
 
-        TicTacToe t = new TicTacToe();
-        Players p1 = new Players(name1, m1);
-        Players p2 = new Players(name2, m2);
+        if(choice==1){
 
-        Players current;
-        if(p1.mark == 'X'){
-            current = p1;
-        }else{
-            current = p2;
-        }
-
-        do{
-            System.out.println(current.name + "'s turn:");
-            current.move();
-            TicTacToe.diaplayBoard();
-            if(TicTacToe.checkWin()){
-                System.out.println(current.name + " WON!");
-                break;
+            System.out.println("Enter Player 1 Name: ");
+            sc.nextLine();
+            String name1 = sc.nextLine();
+            System.out.println("Enter Player 1 Mark: ");
+            String temp = sc.nextLine().toUpperCase();
+            char m1 = temp.charAt(0);
+            System.out.println("Enter Player 2 Name: ");
+            String name2 = sc.nextLine();
+            char m2;
+            if(m1 == 'X'){
+                m2 = 'O';
             }else{
-                if(current == p1){
-                    current = p2;
-                }else{
-                    current = p1;
-                }
+                m2 = 'X';
             }
-            if(TicTacToe.checkFull()){
-                System.out.println("IT'S A TIE");
-                break;
-            }
-        }while(true);
 
+            TicTacToe t = new TicTacToe();
+            Players p1 = new Players(name1, m1);
+            Players p2 = new Players(name2, m2);
+
+            Players current;
+            if(p1.mark == 'X'){
+                current = p1;
+            }else{
+                current = p2;
+            }
+
+            do{
+                System.out.println(current.name + "'s turn:");
+                current.move();
+                TicTacToe.diaplayBoard();
+                if(TicTacToe.checkWin()){
+                    System.out.println(current.name + " WON!");
+                    break;
+                }else{
+                    if(current == p1){
+                        current = p2;
+                    }else{
+                        current = p1;
+                    }
+                }
+                if(TicTacToe.checkFull()){
+                    System.out.println("IT'S A TIE");
+                    break;
+                }
+            }while(true);
+
+        }else if(choice==2){
+
+            numberGuess g = new numberGuess();
+            g.start();
+
+        }
         sc.close();
         
     }
---
+++ GameZone/numberGuess.java
@@ -0,0 +1,34 @@
+package GameZone;
+import java.util.*;
+
+class numberGuess{
+    Scanner sc = new Scanner(System.in);
+    Random r = new Random();
+    int num;
+    numberGuess(){
+        num = r.nextInt(100) + 1;
+    }
+    void start(){
+        System.out.println("\n----------------RULES----------------");
+        System.out.println("1: YOU HAVE TO GUESS A NUMBER FROM 1 TO 100");
+        System.out.println("2: YOU WILL HAVE 6 TRYS BEFORE GAME ENDS\n");
+        int guess;
+        int count = 0;
+        while(count!=6){
+            System.out.print("Enter a number: ");
+            guess = sc.nextInt();
+            if(guess==num){
+                System.out.println("\n--------YOU WIN--------\n");
+                break;
+            }else if(num>guess){
+                System.out.println("GUESS SOMETHING A BIT HIGHER!");
+            }else if(num<guess){
+                System.out.println("GUESS SOMETHING A BIT LOWER!");
+            }
+            count++;
+        }
+        System.out.println("\nGAME ENDED");
+        System.out.println("THE NUMBER WAS : " + num);
+    }
+
+}
